2a74c1d8429cf308d408e63de917cbdba6a9d5f0
Ndene SENE <ndenenafi10@gmail.com>
2020-03-05 12:47:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ZipInfo PWA
+
+> Zipcode city finder PWA built with Vue.js and Ionic 4 using the @ionic/vue package and the @vue/pwa plugin
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipInfo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch v-on:get-zip="getZipInfo" />
+      <ZipInfo v-bind:info="info" />
+      <ClearInfo v-bind:info="info" v-on:clear-info="clearInfo" />
+      <span v-if="info">
+        <router-link to="/about" color="light" expand="block">About</router-link>
+      </span>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
+import ClearInfo from "../components/ClearInfo";
+
+export default {
+  name: "home",
+  components: { ZipSearch, ZipInfo, ClearInfo },
+  data() {
+    return {
+      info: null
+    };
+  },
+  methods: {
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
+      if (res.status == 404) {
+        this.showAlert();
+      }
+      this.info = await res.json();
+    },
+    clearInfo() {
+      this.info = null;
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Not Valid",
+          message: "Please enter a valid US zipcode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
+    }
+  }
+};
+</script>
